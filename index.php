e8e94ebe4fbcf18a03a635e9b9bb08af6d32feea
talkoot <info@talkoot.fi>
2015-03-22 15:35:08 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,266 @@
+<?php include 'header.html';?>
+
+<!-- wtf is this?! :) 
+    <script>
+    var p = $( "li:first" );
+    $( "li:last" ).text( "scrollTop:" + p.scrollTop() );
+    </script>
+-->
+
+<div style="height: 100%"></div>
+<video autoplay loop poster="polina.jpg" id="bgvid">
+	<source src="taustavideo.mp4" type="video/mp4">
+</video>
+
+<style>
+video#bgvid {
+	position: absolute; right: 0; bottom: 0;
+	min-width: 100%; min-height: 100%;
+	width: auto; height: auto; z-index: -100;
+	background: url(polina.jpg) no-repeat;
+	background-size: cover;
+}
+</style>
+
+<!-- OLD VIDEO MEREEN
+<script src="//vjs.zencdn.net/4.3/video.js"></script>
+<script src="http://campusseminar.com/wp-content/themes/Campus/js/imagesloaded.pkgd.min.js"></script>
+<script src="http://campusseminar.com/wp-content/themes/Campus/js/bigvideo.js"></script>
+
+<script type="text/javascript">
+ 
+  $j=jQuery.noConflict();
+ 
+  $j(document).ready(function($) {
+                
+      $(function() {
+          var BV = new $.BigVideo();
+          BV.init();
+          BV.show([
+              { 
+              type: 'video/mp4', 
+              src: 'taustavideo.mp4' 
+              }, 
+              { 
+              type: 'video/webm', 
+              src: 'http://campusseminar.com/wp-content/themes/Campus/videos/20141008_DREAMDO_Campus_seminaari_20s_1_online.webmhd.webm' 
+              }, 
+              { 
+              type: 'video/ogg', 
+              src: 'http://campusseminar.com/wp-content/themes/Campus/videos/20141008_DREAMDO_Campus_seminaari_20s_2_online.ogv' 
+              }
+          ], 
+          {ambient:true});
+      });
+    
+  });
+  
+</script>
+--> 
+
+    <section id="header">
+
+      <div id="header-contents">
+
+            <h1 class="text-center"><img src="tp-logo.png" /></h1>
+            <a href="#talkoopaiva" class="btn btn-lg btn-primary">Lue lisää</a>
+      </div>
+
+    </section>
+
+
+    <section id="talkoopaiva">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h1>Talkoopäivä on yhdessä tekemisen päivä</h1>
+            <br><br>
+          </div>
+          
+          <div class="col-md-5 col-md-offset-1">
+            <p class="lead">Talkoopäivänä kuka tahansa voi järjestää talkoot. Tekeminen voi liittyä esimerkiksi harrastukseen, ongelman ratkaisuun tai uuden asian oppimiseen. Talkoopäivän tavoitteena on elvyttää talkooperinnettä ja saada suomalaiset tekemään yhdessä.</p>
+            <p class="lead">Talkoopäivänä tuhannet ihmiset ympäri Suomea käärivät hihat yhteisen hyvän puolesta.</p>
+          </div>
+
+          <div class="col-md-5">
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe src="//www.youtube.com/embed/AFS_fMQwoI8" class="embed-responsive-item" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+            </div> 
+          </div>
+
+
+
+          <div class="col-md-8 col-md-offset-2 text-center" id="mediat">
+            <div class="row">
+              <div class="col-md-3"><a href="http://www.hs.fi/kotimaa/a1392091411484"><img src="hs.png" class="img-responsive"></a></div>
+              <div class="col-md-3"><a href="https://www.youtube.com/watch?v=p8PO1hl0o0A"><img src="mtv.png" class="img-responsive"></a></div>
+              <div class="col-md-3"><a href="http://ohjelmaopas.yle.fi/1-2278425" target="_blank"><img src="yle.png" class="img-responsive"></a></div>
+              <div class="col-md-3"><a href="http://www.hameensanomat.fi/uutiset/meno-paalla/261132-talkoopaiva-elvyttaa-hyvaa-perinnetta"><img src="hasa.png" class="img-responsive"></a></div>
+            </div>            
+          </div>
+
+        </div>
+      </div>
+      
+    </section>
+
+
+
+    <section id="esimerkkeja">
+
+      <div class="example" style="background-image: url(temp1.png);">
+        Lorem ipsum
+      </div>
+
+      <div class="example example-alt" style="background-image: url(temp2.png);">
+        Lorem ipsum
+      </div>
+
+      <div class="example" style="background-image: url(temp3.png);">
+        Lorem ipsum
+      </div>
+
+      <div class="example example-alt" style="background-image: url(temp4.png);">
+        Lorem ipsum
+      </div>
+
+      <!--
+
+      <div class="example example-alt">
+        Lorem ipsum
+      </div>
+
+      <div class="example">
+        Lorem ipsum
+      </div>
+
+      <div class="example example-alt">
+        Lorem ipsum
+      </div>
+
+      <div class="example">
+        Lorem ipsum
+      </div>
+    
+      -->
+
+
+    </section>
+
+
+
+    <section id="jarjesta">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-md-offset-3 text-center">
+            <h1>Järjestä talkoot 16.5.</h1>
+            <p class="lead">Talkoot on ainutlaatuinen tapa kerätä porukka koolle, osallistua, pitää hauskaa ja luoda yhdessä. Tule mukaan!</p>
+            <a href="?s=jarjesta" class="btn btn-lg btn-primary">Järjestä talkoot</a>
+
+            <p><br><br><a href="?s=talkoot">Lue lisää talkoista</a></p>
+          </div>
+        </div>
+      </div>
+      
+    </section>
+
+    <section id="side-header">
+
+      <div id="side-header-contents">
+
+            <h1>Talkoohenki luo hyvinvointia</h1>
+      </div>
+
+    </section>
+
+
+    <section id="talkoot">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 text-center">
+            <h1>Yhdessä saadaan aikaan enemmän</h1>
+            <br><br>            
+            <p class="lead">Talkoot on työn tekemistä vapaaehtoisesti ja yhdessä. Sen tavoitteena on synnyttää yhteistä hyvää sekä vahvistaa yhteishenkeä. Vapaaehtoisesti ja iloisella mielellä tehtävä talkootyö voi olla esimerkiksi roskien keräämistä luonnosta, saunan rakentamista tai vaikka urheiluturnauksen järjestäminen.</p>
+
+          </div>
+          
+
+
+
+
+
+        </div>
+      </div>
+      
+    </section>
+
+    <section id="kuva" style="background-image: url(http://eralehti.fi/wp-content/uploads/era/images/Arin_kalahatusta/Elonkorjuutalkoot.jpg);">
+    </section>
+
+
+    <section id="historia">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h1>Vuosisatoja vanha perinne</h1>
+            <br><br>
+          </div>
+
+
+          <div class="col-md-5 col-md-offset-1">
+            <p class="lead">Talkoot on pohjoisille kansoille ominainen piirre. Ensimmäiset viitteet suomalaiseen talkooperinteeseen löytyvät jo 1500-luvulla julkaistuista historiateoksista, joissa kuvataan kyläyhteisöjen tapaa tehdä suuret maataloustyöt yhdessä ja järjestää juhlat työn päätteeksi. </p>
+            <p class="lead">Kaupungistumisen myötä talkootyö sai uusia muotoja, kun vapaapalokunnat, partiolaisuus, urheiluseurat ja yhteiskunnallinen vaikuttaminen houkuttivat ihmisiä vapaaehtoistyön äärelle.
+              Sotien jälkeisen ajan jälleenrakennustyö, opiskelijatoiminta ja lukuisat harrastusseurat ovat pitäneet talkoohenkeä elossa aina tähän päivään saakka.</p>
+          </div>
+
+          <div class="col-md-4 col-md-offset-1">
+
+            <div class="tiesitko">
+              <h3>Tiesitkö että?</h3>
+              <ul>
+                <li>"Sadonkorjuussa auttavat kaikki auliisti toisiaan ja mahdollisesti syntyneet kiistat samoin kuin työn vaivatkin kuitataan iloisissa pidoissa. Tällaisen sovun vallitessa voidaan useampia peltoja viljellä kaikessa rauhassaan ja saada niistä suurempia satoja." Gothus, 1555, Historia de gentibus septentrionalibus</li>
+                <li>Otaniemen teekkarikylän rakennusten alkuperäiset tiilet tuotiin paikalle Helsingistä, Neuvostoliiton suurlähetystöstä, jota he olivat sodassa itse vahingossa pommittaneet. Ensimmäiset teekkarikylän rakennukset valmistuivat 1952 ja toimivat olympiakylänä.</li>
+              </ul>
+            </div>
+
+          </div>
+
+
+        </div>
+      </div>
+      
+    </section>   
+
+
+    <section id="kuva" style="background-image: url(slush.jpg);">
+
+    </section>
+
+    <section id="nykypaiva">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 text-center">
+            <h1>Talkoohenki elää yhä vahvana</h1>
+            <br><br>
+            <p class="lead">Yhteiskunnan muuttuessa talkooperinne mukautuu edelleen. Talkoohenki on näkynyt vahvasti esimerkiksi suomalaisen peliteollisuuden juurissa, musiikki- ja kaupunkitapahtumien synnyssä ja startup-opiskelijatoiminnassa. Esimerkiksi Slush-tapahtumaa oli mukana järjestämässä yli 1500 vapaaehtoista.
+            Talkoohengen ylläpito vaatii aktiivista työtä ja uusien vapaaehtoisuuden muotojen kunnioitusta.</p>
+            
+            <br><br><br>
+            <p><a href="?s=jarjesta" class="btn btn-lg btn-primary">Järjestä talkoot</a></p>
+
+          </div>
+          
+
+           
+
+        </div>
+      </div>
+      
+    </section>
+
+<?php include 'footer.html';?>
---
+++ kartta.php
@@ -0,0 +1,4 @@
+<?php include 'header.html';?>
+<div style="height: 60px; background-color: black"></div>
+<iframe src="http://messis.fi/ngm" style="width: 100%; height: 95%" frameborder="no"></iframe>
+<?php include 'footer.html';?>
